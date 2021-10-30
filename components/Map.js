c4f0aa9dde4ce07ad236bd55ce4c2c450f81385e
Godfrey <gtwene001@st.ug.edu.gh>
2021-10-30 21:40:23 +0000

    MapMarker

--- components/Map.js
+++ components/Map.js
@@ -1,35 +1,70 @@
-import { useState } from 'react';
-import ReactMapGl from 'react-map-gl';
-import getCenter from 'geolib/es/getCenter'
+import { useState } from "react";
+import ReactMapGl, { Marker, Popup } from "react-map-gl";
+import getCenter from "geolib/es/getCenter";
 
 function Map({ searchResults }) {
+  const [selectedLocation, setSelectedLocation] = useState({});
 
-    // Transform the search into an Object
-    //{latitude: 52.516272, longitude: 13.377722}
-    const coordinate = searchResults.map(result => ({
-        longitude: result.long,
-        latitude: result.lat,
-    }));
+  // Transform the search into an Object
+  //{latitude: 52.516272, longitude: 13.377722}
+  const coordinate = searchResults.map((result) => ({
+    longitude: result.long,
+    latitude: result.lat,
+  }));
 
-    // The longitude and latitude center
-    const center = getCenter(coordinate)
+  // The longitude and latitude center
+  const center = getCenter(coordinate);
 
-    const [viewport, setViewport] = useState({
-        width: '100%',
-        height: '100%',
-        latitude: center.latitude,
-        longitude: center.longitude,
-        zoom: 11,
-    });
+  const [viewport, setViewport] = useState({
+    width: "100%",
+    height: "100%",
+    latitude: center.latitude,
+    longitude: center.longitude,
+    zoom: 11,
+  });
 
-    return (
-        <ReactMapGl
-            mapStyle='mapbox://styles/gkwame/ckvbi9rogchzd14s7c4fd6y0q'
-            mapboxApiAccessToken={process.env.mapbox_key}
-            {...viewport}
-            onViewportChange={(nextViewport) => setViewport(nextViewport)}
-        ></ReactMapGl>
-    )
+  return (
+    <ReactMapGl
+      mapStyle="mapbox://styles/gkwame/ckvbi9rogchzd14s7c4fd6y0q"
+      mapboxApiAccessToken={process.env.mapbox_key}
+      {...viewport}
+      onViewportChange={(nextViewport) => setViewport(nextViewport)}
+    >
+      {searchResults.map((result) => (
+        <div key={result.long}>
+          <Marker
+            longitude={result.long}
+            latitude={result.lat}
+            offsetLeft={-20}
+            offsetTop={-10}
+          >
+            <p
+              role="img"
+              onClick={() => setSelectedLocation(result)}
+              className="cursor-pointer text-2xl animate-bounce"
+              arial-label="push-pin"
+            >
+              🎈
+            </p>
+          </Marker>
+
+          {/* The popup that should show when clicked on a marker */}
+          {selectedLocation.long === result.long ? (
+            <Popup
+              onClose={() => setSelectedLocation({})}
+              closeOnClick={true}
+              latitude={result.lat}
+              longitude={result.long}
+            >
+              {result.title}
+            </Popup>
+          ) : (
+            false
+          )}
+        </div>
+      ))}
+    </ReactMapGl>
+  );
 }
 
-export default Map
+export default Map;
